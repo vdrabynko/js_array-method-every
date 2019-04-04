2e3f697b86198620d4108d6e1199588d70b85727
Vadim Ilchenko <v.ilchenko@mate.academy>
2019-04-04 15:49:00 +0300

    implemented task and tests

---
+++ src/arrayMethodEvery.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * Implement method Filter
+ */
+function applyCustomEvery() {
+  [].__proto__.every2 = function() {
+    // write code here
+  };
+}
+
+module.exports = applyCustomEvery;
